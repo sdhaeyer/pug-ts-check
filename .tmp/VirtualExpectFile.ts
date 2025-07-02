ffc395419e199ed27a5147997f5b09b55f548458
SD <frustrul@gmail.com>
2025-07-02 12:23:58 +0200

    remove: tmp from index

--- .tmp/VirtualExpectFile.ts
+++
@@ -1,2 +0,0 @@
-import type { User, Cart } from '../tests/example-test-project/types/models'
-type ExpectContract = { user: User, cart: Cart };
--- tests/example-test-project/.tmp/VirtualExpectFile.ts
+++
@@ -1,2 +0,0 @@
-import type { User, Cart } from '../types/models'
-type ExpectContract = { user: User, cart: Cart };
